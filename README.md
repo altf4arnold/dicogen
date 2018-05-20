06ee02b3d501873d49255879045f50fef39d1173
altf4arnold <arnold.dech@adct.be>
2018-05-20 03:47:04 +0200

    the first commit of the project. Fully functionnal but maybe not optimal

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple wordlist and hash generator
+
+##Dependencies :
+* sqlite3
+* hashlib
+* itertools
+* python 3
+
+##How to run :
+
+To generate the database (be carefull, the database can get very big very fast) :
+```python3 dicogen.py gen```
+
+To run to make a research (you already need the database) :
+```python3 dicogen.py```
---
+++ dicogen.py
@@ -0,0 +1,83 @@
+import sqlite3
+import hashlib
+import sys
+import os
+from itertools import product
+
+def select():
+	#This module is watching if we are asking to generate the dictionnary
+	arguments=sys.argv
+	if len(arguments)==1:
+		return 0
+	else:
+		if arguments[1]=='gen':
+			return 1
+		else:
+			return 10
+
+def databaseinit():
+	#this module will generate the database 
+	os.system("rm -f dico.db")
+	database = sqlite3.connect('dico.db')
+	c=database.cursor()
+	c.execute('''CREATE TABLE words(clear text, sha1 text, sha256 text, md5 text)''')
+	#Here we just created the database and the table.
+	database.commit()
+	c.close()
+	database.close()
+	return 0
+
+def write(words):
+	#Here, we are writing the results and making hashes and writing them to the database
+	database = sqlite3.connect('dico.db')
+	c=database.cursor()
+	#print(words)   #Was used for testing
+	#print(hashlib.sha1(words.encode('utf-8')).hexdigest())  #Was used for testing
+	c.execute("INSERT INTO words (clear,sha1,sha256,md5) VALUES (?,?,?,?)",( words , hashlib.sha1(words.encode('utf-8')).hexdigest() , hashlib.sha256(words.encode('utf-8')).hexdigest() , hashlib.md5(words.encode('utf-8')).hexdigest() ) )
+	database.commit()
+	c.close()
+	database.close()
+
+def allwords(chars, length):
+	#generating words
+    for letters in product(chars, repeat=length):
+        yield ''.join(letters)
+
+def initiator(characters):
+	#Setting up words
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    for wordlen in range(characters[0],characters[1]+1):
+        for word in allwords(letters, wordlen):
+            write(word)
+    print("DONE !")
+
+def generatorcontrolpannel():
+	#Control pannel for the user to reate his database
+	numberofchars=[]
+	numberofchars.append(int(input("Minimum char number : ")))
+	numberofchars.append(int(input("Maximim char number : ")))
+	return numberofchars
+
+def reader():
+	#Module used to read the database
+	research=input("What do you want to look up ? : ")
+	database = sqlite3.connect('dico.db')
+	c=database.cursor()
+	clear = c.execute("SELECT clear FROM words WHERE clear OR sha1 OR sha256 OR md5 LIKE '%s'"%research).fetchone()
+	sha1 = c.execute("SELECT sha1 FROM words WHERE clear OR sha1 OR sha256 OR md5 LIKE '%s'"%research).fetchone()
+	sha256 = c.execute("SELECT sha256 FROM words WHERE clear OR sha1 OR sha256 OR md5 LIKE '%s'"%research).fetchone()
+	md5 = c.execute("SELECT md5 FROM words WHERE clear OR sha1 OR sha256 OR md5 LIKE '%s'"%research).fetchone()
+	print("clear : ",clear[0], "\nsha1 : ", sha1[0], "\nsha256 : ", sha256[0], "\nmd5 : ", md5[0])
+	database.close()
+
+if select()==0:
+	reader()
+elif select()==1:
+	if databaseinit()==0:
+		initiator(generatorcontrolpannel())
+	else:
+		print("Whooops, an error occured while creating the database\n")
+		print("Please verify your permissions")
+
+elif select()==10:
+	print("You've entered bullshit :'(")
